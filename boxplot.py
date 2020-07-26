6dcc7ae3b927ab88df3c050ea8c37a6076ea7b2b
dirkkoolmees <65215053+dirkkoolmees@users.noreply.github.com>
2020-07-26 10:17:55 +0700

    Update boxplot.py

--- boxplot.py
+++ boxplot.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+# Build App
+app = dash.Dash(__name__)
+server = app.server
 
 url = 'https://raw.githubusercontent.com/dirkkoolmees/CO2_emissions_per-region/master/CO2%20Emissions%20per%20region%20-%20Sheet2.csv'
 df = pd.read_csv(url)
@@ -20,7 +26,19 @@ fig = px.box(df, y=df.columns, color_discrete_sequence=px.colors.sequential.RdBu
 fig.update_traces(quartilemethod="inclusive") # or "exclusive", or "linear" by default
 
 fig.update_layout(
-    title='CO2 Emissions from fossil fuels and cement production',
-    yaxis_title='Thousand metric tons of C',
-    xaxis_title=' '
-    )
+        title='CO2 Emissions from fossil fuels and cement production',
+        yaxis_title='Thousand metric tons of C',
+        xaxis_title=' '
+                    )
+
+app.layout = html.Div([
+        html.Div([
+        dcc.Graph(id='graph', figure = fig),
+    ],style={'display': 'inline-block', 'width': '100%'})
+        
+])
+
+
+# Run app
+if __name__ == '__main__':
+    app.run_server()
